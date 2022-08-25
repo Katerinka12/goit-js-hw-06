fc478a9011ad26ddb1a0d8fbed65ec26bef693a0
Katerinka12 <katerinka.kk12@gmail.com>
2022-08-25 13:39:24 +0300

    add 8,9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
 
+const formEl = document.querySelector(".login-form");
+
+formEl.addEventListener('submit', onBtnSubmit);
+
+function onBtnSubmit(event){
+    event.preventDefault();
+    const {elements: {email, password}} = event.currentTarget;
+if( email.value === "" || password.value === "")
+{
+    alert("Please fill in all the fields!");
+}
+const userInfo = { email: email.value, Password: password.value};
+console.log(userInfo);
+event.currentTarget.reset();
+}
